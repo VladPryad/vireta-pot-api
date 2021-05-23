db3f9fee2eed3c85ea4ff6373349f690fa27edd4
Vlad <2000californiadream2000@gmail.com>
2021-05-23 14:27:34 +0300

    Randomizing timestamps in mock WS

--- dist/src/pot/mock/pot-measurement.mock.js
+++ dist/src/pot/mock/pot-measurement.mock.js
@@ -1,6 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const events_1 = require("../constants/events");
+function between(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function compare(a, b) {
+    return a - b;
+}
 function default_1(socket, count, interval, id = "5fg54566v") {
     function delay(i) {
         setTimeout(() => {
@@ -17,38 +23,41 @@ function default_1(socket, count, interval, id = "5fg54566v") {
 exports.default = default_1;
 const mockMeasurement = (id) => {
     const seed = Date.now();
+    let rands = [0, between(0, 100), between(0, 100)];
+    let shift = rands.sort(compare);
+    console.log(shift);
     return {
         potId: id + '',
         potName: `Pot ` + id,
         measurements: {
             ph: [{
-                    timestamp: seed + '',
+                    timestamp: seed + shift[0] + '',
                     value: Math.random() * 10 + ''
                 }, {
-                    timestamp: seed + 10 + '',
+                    timestamp: seed + shift[1] + '',
                     value: Math.random() * 10 + ''
                 }, {
-                    timestamp: seed + 20 + '',
+                    timestamp: seed + shift[2] + '',
                     value: Math.random() * 10 + ''
                 }],
             humidity: [{
-                    timestamp: seed + '',
+                    timestamp: seed + shift[0] + '',
                     value: Math.random() * 10 + ''
                 }, {
-                    timestamp: seed + 10 + '',
+                    timestamp: seed + shift[1] + '',
                     value: Math.random() * 10 + ''
                 }, {
-                    timestamp: seed + 20 + '',
+                    timestamp: seed + shift[2] + '',
                     value: Math.random() * 10 + ''
                 }],
             mineralization: [{
-                    timestamp: seed + '',
+                    timestamp: seed + shift[0] + '',
                     value: Math.random() * 10 + ''
                 }, {
-                    timestamp: seed + 10 + '',
+                    timestamp: seed + shift[1] + '',
                     value: Math.random() * 10 + ''
                 }, {
-                    timestamp: seed + 20 + '',
+                    timestamp: seed + shift[2] + '',
                     value: Math.random() * 10 + ''
                 }]
         },
--- src/pot/mock/pot-measurement.mock.ts
+++ src/pot/mock/pot-measurement.mock.ts
@@ -2,6 +2,15 @@ import PotRecord from "../dto/pot-record.dto";
 import WebSocket from 'ws';
 import { MSG } from "../constants/events";
 
+function between(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+}
+function compare(a, b) {
+    return a - b;
+  }
+
 export default function (socket: WebSocket, count: number, interval: number, id: string = "5fg54566v") {
     function delay(i: number) {
         setTimeout(() => {
@@ -21,38 +30,42 @@ export default function (socket: WebSocket, count: number, interval: number, id:
 const mockMeasurement = (id: string): PotRecord => {
     const seed = Date.now();
 
+    let rands = [0,between(0,100),between(0,100)];
+    let shift = rands.sort(compare);
+    console.log(shift)
+
     return {
         potId: id + '',
         potName: `Pot ` + id,
         measurements: {
             ph: [{
-                timestamp: seed + '',
+                timestamp: seed + shift[0] + '',
                 value: Math.random() * 10 + ''
             }, {
-                timestamp: seed + 10 + '',
+                timestamp: seed + shift[1] + '',
                 value: Math.random() * 10 + ''
             }, {
-                timestamp: seed + 20 + '',
+                timestamp: seed + shift[2] + '',
                 value: Math.random() * 10 + ''
             }],
             humidity: [{
-                timestamp: seed + '',
+                timestamp: seed + shift[0] + '',
                 value: Math.random() * 10 + ''
             }, {
-                timestamp: seed + 10 + '',
+                timestamp: seed + shift[1] + '',
                 value: Math.random() * 10 + ''
             }, {
-                timestamp: seed + 20 + '',
+                timestamp: seed + shift[2] + '',
                 value: Math.random() * 10 + ''
             }],
             mineralization: [{
-                timestamp: seed + '',
+                timestamp: seed + shift[0] + '',
                 value: Math.random() * 10 + ''
             }, {
-                timestamp: seed + 10 + '',
+                timestamp: seed + shift[1] + '',
                 value: Math.random() * 10 + ''
             }, {
-                timestamp: seed + 20 + '',
+                timestamp: seed + shift[2] + '',
                 value: Math.random() * 10 + ''
             }]
         },
